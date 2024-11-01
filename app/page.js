074f1f0e62abd7edad77bc2fbea05ccb4b3f1c4a
anandgarikapati1 <anand.g@financebox.in>
2024-11-01 13:29:23 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1>anand</h1>
+      <h1>anand kumar</h1>
     </div>
   );
 }
